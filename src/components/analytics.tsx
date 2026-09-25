1aab072e5ddd33be0b2878ba8a43e0e81066b15f
urebarif-ctrl <331607748+urebarif-ctrl@users.noreply.github.com>
2026-09-25 23:59:30 +0500

    perf: defer analytics until the browser is idle

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -29,7 +29,7 @@ export function Analytics() {
     <>
       <Script
         id="gtm-script"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
